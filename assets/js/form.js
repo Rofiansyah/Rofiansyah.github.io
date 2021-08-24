fd93be0e87ccf64e3eea9f7d49c26a632d3da9b4
Rofiansyah <wizmanrofiansyah@gmail.com>
2021-08-24 19:22:24 +0700

    menyempurnakan fitur kirim pesan

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,10 +1,34 @@
 // ==================== FORM JS ====================
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyPl6DYLz8aTimibvK6xeB7S4uINPeC6-71EbWlS6787MTjCKioVE1o3ehzJFwxgwVUgg/exec'
 const form = document.forms['notyourwizzz']
+const send = document.querySelector('.button-send')
+const loading = document.querySelector('.button-loading')
+const alert = document.querySelector('.section__title_3')
+
 
   form.addEventListener('submit', e => {
     e.preventDefault()
+
+    // sending
+    loading.classList.toggle('d-none')
+    send.classList.toggle('d-none')
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response => {
+        // loading
+        loading.classList.toggle('d-none')
+        send.classList.toggle('d-none')
+        alert.classList.toggle('d-none')
+
+        // reset form
+        form.reset()
+
+        console.log('Success!', response)
+    })
       .catch(error => console.error('Error!', error.message))
   })
+
+  if(alert){
+    alert.addEventListener('click', () =>{
+      alert.classList.toggle('d-none')
+    })
+  }
